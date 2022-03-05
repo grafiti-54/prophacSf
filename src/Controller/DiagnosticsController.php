42c23bdd2c8342b078331e41ab8d279d41291cd8
grafiti54 <grafiti54@gmail.com>
2022-03-05 22:56:22 +0100

    reglage diagnostics

--- src/Controller/DiagnosticsController.php
+++ src/Controller/DiagnosticsController.php
@@ -6,13 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/diagnostics')]
 class DiagnosticsController extends AbstractController
 {
-    #[Route('/diagnostics', name: 'app_diagnostics')]
-    public function index(): Response
+    //Page diagnostics
+    #[Route('/', name: 'app_diagnostics')]
+    public function accueilDiagnostics(): Response
     {
-        return $this->render('diagnostics/index.html.twig', [
-            'controller_name' => 'DiagnosticsController',
-        ]);
+        return $this->render('diagnostics/diagnostics.html.twig');
+    }
+
+    //Page annuaire diagnostics
+    #[Route('/annuaire', name: 'app_diagnostics.annuaire')]
+    public function annuaireDiagnostics(): Response
+    {
+        return $this->render('diagnostics/annuaire.html.twig');
     }
 }
