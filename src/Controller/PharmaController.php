d6a8d6919520a9ff5e1a222a22cbed95b5564c37
grafiti54 <grafiti54@gmail.com>
2022-03-19 11:52:37 +0100

    recuperation des données en base

--- src/Controller/PharmaController.php
+++ src/Controller/PharmaController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Collaborateurs;
+use App\Entity\Partenaires;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,15 +16,37 @@ class PharmaController extends AbstractController
 {   
     //Page pharmaceuticals
     #[Route('/', name: 'app_pharma')]
-    public function accueilPharma(): Response
+    public function accueilPharma(ManagerRegistry $doctrine): Response
     {
-        return $this->render('pharma/pharmaceuticals.html.twig');
+        /**
+         * Id du département pharmaceuticals
+         */
+        $idDepartement  = 3;
+        /**
+         * Recherche des partenaires liés au départment concerné
+         */
+        $partenaireDepartement = $doctrine->getRepository(Partenaires::class)->findPartenaireByDepartement($idDepartement);
+
+        return $this->render('pharma/pharmaceuticals.html.twig',[
+            'partenaires' => $partenaireDepartement,
+        ]);
     }
 
     //Page annuaire pharmaceuticals
     #[Route('/annuaire', name: 'app_pharma.annuaire')]
-    public function annuairePharma(): Response
+    public function annuairePharma(ManagerRegistry $doctrine): Response
     {
-        return $this->render('pharma/annuaire.html.twig');
+        /**
+         * Id du département pharmaceuticals
+         */
+        $idDepartement  = 3;
+        /**
+         * Recherche des collaborateurs liés au départment concerné
+         */
+        $collaborateurDepartement = $doctrine->getRepository(Collaborateurs::class)->findCollaborateurByDepartement($idDepartement);
+
+        return $this->render('pharma/annuaire.html.twig',[
+            'collaborateurs' => $collaborateurDepartement,
+        ]);
     }
 }
